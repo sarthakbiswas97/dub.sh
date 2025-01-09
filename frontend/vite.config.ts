0edc612d57011e31c651a3edb5f86114e2e480a8
sarthaksbiswas97 <sarthak.sbiswas@gmail.com>
2025-01-10 04:28:23 +0530

    vite config changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // eslint-disable-next-line no-undef
         target: process.env.VITE_API_URL,
         changeOrigin: true,
       }
     }
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src') // Add this alias configuration
+    }
+  }
 })

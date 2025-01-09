e2e211b26e04bd67dbb83741d58cb181571821e5
sarthaksbiswas97 <sarthak.sbiswas@gmail.com>
2025-01-10 04:21:03 +0530

    Add CI/CD workflows and Docker configurations

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,15 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  preview: {
-    host: '0.0.0.0',
-    port: 4173
+  server: {
+    proxy: {
+      '/api': {
+        // eslint-disable-next-line no-undef
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+      }
+    }
   },
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 })
